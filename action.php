60157b616afe676b674d2054d6171bb99441d1bf
Hove Gotthard <hovegotthard@web.de>
2018-01-26 19:12:29 +0100

    .css complete

---
+++ action.php
@@ -0,0 +1,20 @@
+<?php
+if ($_GET["action"] == "1") {
+    echo exec('echo "on 0" | cec-client -s -d 1');
+    header('Location: /index.php');
+}
+if ($_GET["action"] == "2") {
+    echo exec('echo "standby 0" | cec-client -s -d 1');
+    header('Location: /index.php');
+}
+if ($_GET["action"] == "3") {
+    echo exec('echo u > /var/run/omxctl');
+    echo exec('echo p > /var/run/omxctl');
+    echo exec('echo h > /var/run/omxctl');
+    header('Location: /index.php');
+}
+if ($_GET["action"] == "4") {
+    echo exec('echo p > /var/run/omxctl');
+    header('Location: /index.php');
+}
+?> 
