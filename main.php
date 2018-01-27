e33a8fdd87245ea4ffab1c954f4ed47fbd460d36
Hove Gotthard <hovegotthard@web.de>
2018-01-27 13:39:03 +0100

    Remove seperate pages
    Removed musik_on.php and tv_on.php

--- main.php
+++ main.php
@@ -6,8 +6,10 @@
       $result = $con -> query($sql);
       $row = $result->fetch_assoc();
       if ($row["id"]) {
-      include 'TV_on.php';
-      include 'Musik_on.php';
+      echo exec('echo "on 0" | cec-client -s -d 1');
+      echo exec('echo u > /var/run/omxctl');
+      echo exec('echo p > /var/run/omxctl');
+      echo exec('echo h > /var/run/omxctl');
       
         $sql="delete from alarm where id=".$row["id"];
         $con -> query($sql);
